84c5027a3e659de1027b66bf7f94ededf2bff720
nanditaupreti <nanditaupreti312@gmail.com>
2023-09-23 20:32:17 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 import Home from './pages/Home/Home'
 import Start from './pages/Yoga/Start'
-
+import Yoga from './pages/Yoga/Yoga'
 import About from './pages/About/About'
 import Tutorials from './pages/Tutorials/Tutorials'
 
@@ -14,7 +14,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/start' element={<Start />} />
+        <Route path='/start' element={<Yoga />} />
         <Route path='/about' element={<About />} />
         <Route path='/tutorials' element={<Tutorials />} />
       </Routes>
